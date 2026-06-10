450dcca163dabffc414b8f36489f2aabec4523b3
prog_Lavanya <lavanyasingh3804@gmail.com>
2026-06-10 20:33:03 +0530

    Remove ODBC specific args

--- backend/database.py
+++ backend/database.py
@@ -7,15 +7,8 @@ load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in the .env file")
-engine=create_engine(
+engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "attrs_before":{
-            1222:1
-        }
-    },
-    pool_size=10,
-    max_overflow=20,
     pool_pre_ping=True
 )
 SessionLocal =sessionmaker(autocommit=False,autoflush=False,bind=engine)
